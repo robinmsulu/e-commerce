45071f8fdd450ec1cdb32bf79c4d05b65fc0ab6b
ROBBIE <mwirigirobin8@gmail.com>
2023-11-18 14:04:26 +0300

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDwvSV4D3_p9J420eXnYdMnMJ4zsigmIwo",
+  authDomain: "netme-fb9e6.firebaseapp.com",
+  projectId: "netme-fb9e6",
+  storageBucket: "netme-fb9e6.appspot.com",
+  messagingSenderId: "493419935457",
+  appId: "1:493419935457:web:9cc216657766afbd0969a0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
